c464e3bf9278b3f844605ab0837002b755151b96
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2016-10-09 12:44:54 -0700

    round to eliminate js float errors

--- app/lib/eqDivsOfSet.js
+++ app/lib/eqDivsOfSet.js
@@ -2,11 +2,11 @@ import range from '../helpers/range'
 
 const maxOfSet = set => Math.max.apply(Math, set)
 
-//equal divisors range
-const eqDivRange = set => range(maxOfSet(set) - 1, 2)
+//range of potential equal divisors
+const potEqDivRange = set => range(maxOfSet(set) - 1, 2)
 
 //does this n divide evenly into each element of the set?
 const isEqDivOfSet = (set, n) => set.every(el => el % n === 0)
 
 //equal divisors of set
-export default set => eqDivRange(set).filter(n => isEqDivOfSet(set, n))
+export default set => potEqDivRange(set).filter(n => isEqDivOfSet(set, n))
--- app/lib/normalOrderCount.js
+++ app/lib/normalOrderCount.js
@@ -5,4 +5,4 @@ import permCountWre from './permCountWre'
 //perm count with redundancies eliminated per element
 const permCountWrepe = set => permCountWre(set) / elementCount(set)
 
-export default set => permCountWrepe(set) + sumAcrSetReds(permCountWrepe, set)
+export default set => Math.round(permCountWrepe(set) + sumAcrSetReds(permCountWrepe, set))

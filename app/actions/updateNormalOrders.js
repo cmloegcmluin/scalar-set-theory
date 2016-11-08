69cb4515657f26a7f538f8485f81119de17d9ae8
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2016-11-07 18:47:18 -0800

    normal orders come from backend

--- app/actions/updateNormalOrders.js
+++ app/actions/updateNormalOrders.js
@@ -1,11 +1,9 @@
 import normalOrdersList from '../lib/normalOrdersList'
+import fetch from 'isomorphic-fetch'
+import queryString from 'query-string'
 
 export default async (dispatch, scalarSet) => {
-	const normalOrdersPromise = new Promise(resolve => {
-		setTimeout(() => {
-			resolve(normalOrdersList.getList(scalarSet))
-		}, 0)
-	})
-	const data = await normalOrdersPromise
+	const rawData = await fetch(`http://localhost:3000/normal-orders/${queryString.stringify(scalarSet)}`)
+	const data = await rawData.json()
 	dispatch({type: 'UPDATE_NORMAL_ORDERS', data})
 }

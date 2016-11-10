9584688170d1f1049a9cf6198b573388d48a958e
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2016-11-09 23:36:16 -0800

    gate backend on env

--- app/actions/updateNormalOrders.js
+++ app/actions/updateNormalOrders.js
@@ -2,8 +2,10 @@ import normalOrdersList from '../lib/normalOrdersList'
 import fetch from 'isomorphic-fetch'
 import queryString from 'query-string'
 
+const apiUrl = process.env.NODE_ENV === 'production' ? 'https://scalar-set-theory-backend.cfapps.io' : 'http://localhost:3000';
+
 export default async (dispatch, scalarSet) => {
-	const rawData = await fetch(`http://localhost:3000/normal-orders/${queryString.stringify(scalarSet)}`)
+	const rawData = await fetch(`${apiUrl}/normal-orders/${queryString.stringify(scalarSet)}`)
 	const data = await rawData.json()
 	dispatch({type: 'UPDATE_NORMAL_ORDERS', data})
 }

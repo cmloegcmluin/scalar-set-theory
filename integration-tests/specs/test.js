bcd9f9467c40fc6e84278af87cd18c8ca3c8bb0a
Kyle Gearhart <kgearhart@pivotal.io>
2017-06-13 11:05:54 +0900

    adds a hard coded table
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- integration-tests/specs/test.js
+++ integration-tests/specs/test.js
@@ -1,8 +1,17 @@
 let assert = require('assert');
 
 describe('Scalar Set Theory', function () {
-	it('shows the title', function () {
+	it('shows the title and a table', function () {
 		browser.url('/src/ScalarSetTheory/Main.elm');
-		assert.ok(browser.getHTML('body').includes('Scalar Set Theory'));
+		assert.equal(browser.getText('h1'), 'Scalar Set Theory');
+
+		assert.equal(browser.getText('//table/tr[1]/th[1]'), 'section');
+		assert.equal(browser.getText('//table/tr[1]/th[2]'), 'ed');
+		assert.equal(browser.getText('//table/tr[2]/td[1]'), 'max');
+		assert.equal(browser.getText('//table/tr[2]/td[2]'), '7');
+		assert.equal(browser.getText('//table/tr[3]/td[1]'), 'min');
+		assert.equal(browser.getText('//table/tr[3]/td[2]'), '7');
+		assert.equal(browser.getText('//table/tr[4]/td[1]'), 'count (1)');
+		assert.equal(browser.getText('//table/tr[4]/td[2]'), '7');
 	});
 });
--- src/ScalarSetTheory/Main.elm
+++ src/ScalarSetTheory/Main.elm
@@ -1,7 +1,33 @@
 module ScalarSetTheory.Main exposing (..)
 
-import Html exposing (text)
+import Html exposing (div, h1, table, td, text, th, tr)
 
 
 main =
-    text "Scalar Set Theory"
+    div
+        []
+        [ h1 [] [ text "Scalar Set Theory" ]
+        , table
+            []
+            [ tr
+                []
+                [ th [] [ text "section" ]
+                , th [] [ text "ed" ]
+                ]
+            , tr
+                []
+                [ td [] [ text "max" ]
+                , td [] [ text "7" ]
+                ]
+            , tr
+                []
+                [ td [] [ text "min" ]
+                , td [] [ text "7" ]
+                ]
+            , tr
+                []
+                [ td [] [ text "count (1)" ]
+                , td [] [ text "7" ]
+                ]
+            ]
+        ]

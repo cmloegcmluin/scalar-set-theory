483904f2dbd943711787787db369791a7fc34adc
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-06-14 15:40:13 +0900

    dry up integration test script; btw, a couple commits ago we needed to actually add selenium-standalone to the integration tests
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- integration-tests/specs/test.js
+++ integration-tests/specs/test.js
@@ -1,8 +1,11 @@
 let assert = require('assert');
 
+beforeEach('visit main page', function () {
+	browser.url('/src/ScalarSetTheory/Main.elm');
+})
+
 describe('landing page', function () {
 	it('shows the title and a table', function () {
-		browser.url('/src/ScalarSetTheory/Main.elm');
 		assert.equal(browser.getText('h1'), 'Scalar Set Theory');
 
 		assert.equal(browser.getText('//table/tr[1]/th[1]'), 'section');
@@ -16,7 +19,6 @@ describe('range of equal divisions', function () {
 	it('updates the rows and count of rows when min or max filter is changed', function () {
 		let minSelect = '//table/tr[2]/td[2]/select'
 		let maxSelect = '//table/tr[3]/td[2]/select'
-		browser.url('/src/ScalarSetTheory/Main.elm');
 
 		assert.equal(browser.getValue(minSelect), '2');
 		assert.equal(browser.getValue(maxSelect), '2');
@@ -49,7 +51,9 @@ describe('range of equal divisions', function () {
 const selectOption = function (selectXPath, option) {
 	browser.click(selectXPath + "/option[contains(.,\"" + option + "\")]");
 	browser.execute(function(selectXPath) {
-		const select = document.evaluate(selectXPath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+		const select = document.evaluate(
+			selectXPath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null
+		).singleNodeValue;
 		const event = new Event('input');
 		select.dispatchEvent(event);
 	}, selectXPath);

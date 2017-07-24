9b44fe1088932f49585bb63b20badb77eba1b055
Person McPivot <person.mcpivot@pivotal.io>
2017-07-24 17:11:27 +0900

    bring back update test oops

--- tests/src/ScalarSetTheoryTests/UpdateTests.elm
+++ tests/src/ScalarSetTheoryTests/UpdateTests.elm
@@ -11,44 +11,69 @@ import Test exposing (Test, describe, test)
 
 updateTests : Test
 updateTests =
-    describe "update" []
+    describe "update"
+        [ test "UpdateSectionMin updates the min for the correct section" <|
+            \() ->
+                let
+                    expected =
+                        { sectionData =
+                            [ { section = Ed
+                              , min = 5
+                              , max = 7
+                              }
+                            , { section = NChord
+                              , min = 4
+                              , max = 6
+                              }
+                            ]
+                        }
 
+                    actual =
+                        update
+                            (UpdateSectionMin 5 Ed)
+                            { sectionData =
+                                [ { section = Ed
+                                  , min = 7
+                                  , max = 7
+                                  }
+                                , { section = NChord
+                                  , min = 4
+                                  , max = 6
+                                  }
+                                ]
+                            }
+                in
+                equal expected actual
+        , test "UpdateSectionMax updates the max for the correct section" <|
+            \() ->
+                let
+                    expected =
+                        { sectionData =
+                            [ { section = Ed
+                              , min = 5
+                              , max = 7
+                              }
+                            , { section = NChord
+                              , min = 4
+                              , max = 8
+                              }
+                            ]
+                        }
 
-
---        [ test "UpdateSectionMin updates the min for the correct section" <|
---            \() ->
---                let
---                    expected =
---                        { ed = { min = 5, max = 7 }
---                        , nChord = { min = 4, max = 6 }
---                        , activeSections = [ Ed, NChord ]
---                        }
---
---                    actual =
---                        update
---                            (UpdateSectionMin 5 Ed)
---                            { ed = { min = 7, max = 7 }
---                            , nChord = { min = 4, max = 6 }
---                            , activeSections = [ Ed, NChord ]
---                            }
---                in
---                equal expected actual
---        , test "UpdateSectionMax updates the max for the correct section" <|
---            \() ->
---                let
---                    expected =
---                        { ed = { min = 5, max = 7 }
---                        , nChord = { min = 4, max = 8 }
---                        , activeSections = [ Ed, NChord ]
---                        }
---
---                    actual =
---                        update
---                            (UpdateSectionMax 8 NChord)
---                            { ed = { min = 5, max = 7 }
---                            , nChord = { min = 4, max = 6 }
---                            , activeSections = [ Ed, NChord ]
---                            }
---                in
---                equal expected actual
---        ]
+                    actual =
+                        update
+                            (UpdateSectionMax 8 NChord)
+                            { sectionData =
+                                [ { section = Ed
+                                  , min = 5
+                                  , max = 7
+                                  }
+                                , { section = NChord
+                                  , min = 4
+                                  , max = 6
+                                  }
+                                ]
+                            }
+                in
+                equal expected actual
+        ]

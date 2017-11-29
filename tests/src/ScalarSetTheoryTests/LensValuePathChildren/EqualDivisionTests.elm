1cfc8a8919841be826c94b9f66f0e7e09959b810
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-11-29 11:44:32 +0900

    Revert "Revert "at least one lens unit test""
    This reverts commit 20a08cfe5ea4db9692be91b4b8399ac65b33d711.

--- tests/src/ScalarSetTheoryTests/LensValuePathChildren/EqualDivisionTests.elm
+++ tests/src/ScalarSetTheoryTests/LensValuePathChildren/EqualDivisionTests.elm
@@ -1,21 +1,33 @@
 module ScalarSetTheoryTests.LensValuePathChildren.EqualDivisionTests exposing (equalDivisionTests)
 
 import Expect exposing (equal)
+import ScalarSetTheory.Lens.Lens exposing (Lens(EqualDivision, NChord))
+import ScalarSetTheory.LensSettingValues.LensSettingValues exposing (LensSettingValues)
 import ScalarSetTheory.LensValuePathChildren.EqualDivision exposing (equalDivisionChildrenGetter)
+import ScalarSetTheory.LensValuePathChildren.LensValuePathChildren exposing (LensValuePathChildren)
+import ScalarSetTheory.LensValueStep.LensValueStep exposing (LensValueStep)
+import ScalarSetTheory.Setting.Setting exposing (Setting(Max, Min))
+import ScalarSetTheory.SettingValue.SettingValue exposing (SettingValue)
 import Test exposing (Test, describe, test)
 
 
 equalDivisionTests : Test
 equalDivisionTests =
     describe "equal division child values getter"
-        [ test "" <|
+        [ test "gives the equal divisions which support the value for n-chord and other lenses that its within" <|
             \() ->
                 let
                     expected =
-                        ""
+                        LensValuePathChildren EqualDivision [ "3", "4", "5", "6", "7", "8", "9" ]
+
+                    lensValuePath =
+                        [ LensValueStep NChord "3" ]
+
+                    equalDivisionSettingValues =
+                        LensSettingValues EqualDivision [ SettingValue Min 2, SettingValue Max 9 ]
 
                     actual =
-                        ""
+                        equalDivisionChildrenGetter lensValuePath equalDivisionSettingValues
                 in
                 equal expected actual
         ]

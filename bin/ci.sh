81b6dd5cd3d89c74bca1b6c7bbdde7f8eaa3808b
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-12-08 13:56:43 +0900

    temporarily turn off unit tests on ci because this is insane how long they take

--- bin/ci.sh
+++ bin/ci.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
 mkdir node_modules
-npm i elm elm-test
+npm i elm
+#npm i elm-test
 export PATH=${PATH}:/home/travis/build/cmloegcmluin/scalar-set-theory/node_modules/.bin
 
-elm-test init
-rm /home/travis/build/cmloegcmluin/scalar-set-theory/tests/Example.elm
+#elm-test init
+#rm /home/travis/build/cmloegcmluin/scalar-set-theory/tests/Example.elm
 
-elm-package install -y
+#elm-package install -y
 
-elm-test
+#elm-test
 
 elm-reactor -p 8001 &
 cd integration-tests

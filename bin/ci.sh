603053c42e2a8eb97ceb19223d67f7c847fe940f
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-12-08 15:00:54 +0900

    reverting a crapton of stuff until we get back to where we were seeing elm at all

--- bin/ci.sh
+++ bin/ci.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
 mkdir node_modules
-npm i elm
-#npm i elm-test
+npm i elm elm-test
 export PATH=${PATH}:/home/travis/build/cmloegcmluin/scalar-set-theory/node_modules/.bin
 
-#elm-test init
-#rm /home/travis/build/cmloegcmluin/scalar-set-theory/tests/Example.elm
+elm-test init
+rm /home/travis/build/cmloegcmluin/scalar-set-theory/tests/Example.elm
 
 elm-package install -y
 
-#elm-test
-elm-make /home/travis/build/cmloegcmluin/scalar-set-theory/src/ScalarSetTheory/Main.elm --output /home/travis/build/cmloegcmluin/scalar-set-theory/dist/index.html
+elm-test
 
 elm-reactor -p 8001 &
 cd integration-tests

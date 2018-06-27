7c0f42b4e5d3bcc81a733bdfc975d635a2d884c4
Douglas Blumeyer <dblumeyer@pivotal.io>
2018-06-27 16:45:01 -0700

    been a minute, maybe travis will magically work now

--- bin/ci.sh
+++ bin/ci.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 
 mkdir node_modules
-npm i elm elm-test
-export PATH=${PATH}:/home/travis/build/cmloegcmluin/scalar-set-theory/node_modules/.bin
-
-elm-test init
-rm /home/travis/build/cmloegcmluin/scalar-set-theory/tests/Example.elm
+npm i elm
 
 elm-package install -y
 
-elm-test
-
+elm-reactor -p 8001 &
 cd integration-tests
 npm i
-elm-reactor -p 8001 &
 npm test

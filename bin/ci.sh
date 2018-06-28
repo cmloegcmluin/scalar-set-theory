cf0cb466cec85a9cc589b709b51db20aa524cb5e
Douglas Blumeyer <dblumeyer@pivotal.io>
2018-06-27 17:26:13 -0700

    i hate this

--- bin/ci.sh
+++ bin/ci.sh
@@ -2,11 +2,10 @@
 
 mkdir node_modules
 npm i elm
-export PATH=${PATH}:/home/travis/build/cmloegcmluin/scalar-set-theory/node_modules/.bin
 
 elm-package install -y
 
-elm-make src/ScalarSetTheory/Main.elm --output dist/index.html
+elm-make /home/travis/build/cmloegcmluin/scalar-set-theory/src/ScalarSetTheory/Main.elm --output /home/travis/build/cmloegcmluin/scalar-set-theory/dist/index.html
 elm-reactor -p 8001 &
 cd integration-tests
 npm i

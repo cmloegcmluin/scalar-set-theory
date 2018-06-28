b9883918c8f7c2604d6e7c64b1b869dc4426d25d
Douglas Blumeyer <dblumeyer@pivotal.io>
2018-06-28 12:26:13 -0700

    undo all the other bs and cross fingers

--- bin/ci.sh
+++ bin/ci.sh
@@ -1,30 +1,9 @@
 #!/usr/bin/env bash
 
-echo "installing all sorts of crap"
-sudo apt-get update \
-  && sudo apt-get install -y \
-    python \
-    gcc \
-    make \
-    g++ \
-    build-essential \
-#  && npm i -g node-gyp
-  echo "finished installing crap"
-
-which build-essential
-which g++
-which gcc
-
-elm-package install -y
-
 elm-reactor -p 8001 &
 
 cd integration-tests
 
-echo "node version???"
-node -v
-nodejs -v
-#npm i fibers
 npm i
 
 npm test

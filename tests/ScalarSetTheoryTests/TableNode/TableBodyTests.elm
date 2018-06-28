5492e0be097f4a942ec31abf979ba675b9cf5992
Douglas Blumeyer <dblumeyer@pivotal.io>
2018-06-28 15:05:17 -0700

    table body unit test when lens setting values exist

--- tests/ScalarSetTheoryTests/TableNode/TableBodyTests.elm
+++ tests/ScalarSetTheoryTests/TableNode/TableBodyTests.elm
@@ -2,7 +2,10 @@ module ScalarSetTheoryTests.TableNode.TableBodyTests exposing (tableBodyTests)
 
 import Expect exposing (equal)
 import Html exposing (text)
+import ScalarSetTheory.Css.TableStyles exposing (defaultGrey)
+import ScalarSetTheory.LensProperties.LensProperties exposing (getLensProperties)
 import ScalarSetTheory.Model exposing (Model)
+import ScalarSetTheory.TableCell.TableCell exposing (TableCell)
 import ScalarSetTheory.TableNode.TableBody exposing (tableBody)
 import ScalarSetTheory.TableNode.TableNode exposing (TableNode(TableNode))
 import ScalarSetTheory.Types.Lens exposing (Lens(NChord))
@@ -23,6 +26,36 @@ tableBodyTests =
 
                     model =
                         Model []
+
+                    actual =
+                        tableBody model
+                in
+                equal expected actual
+        , test "when there are active lens setting values" <|
+            \() ->
+                let
+                    expected =
+                        TableNode
+                            { cellItself = Just (TableCell (text "root") defaultGrey)
+                            , cellChildren =
+                                [ TableNode
+                                    { cellItself = Just (TableCell (text "1") defaultGrey)
+                                    , cellChildren =
+                                        [ TableNode
+                                            { cellItself = Just (TableCell (text "0") (getLensProperties NChord).color)
+                                            , cellChildren = []
+                                            }
+                                        ]
+                                    }
+                                ]
+                            }
+
+                    model =
+                        Model
+                            [ { lens = NChord
+                              , settingValues = []
+                              }
+                            ]
 
                     actual =
                         tableBody model

384ec0584da7fa2e42aa27209485fd8885a0eb03
Douglas Blumeyer <dblumeyer@pivotal.io>
2018-06-28 09:15:03 -0700

    try globally installing node-gyp

--- bin/ci.sh
+++ bin/ci.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-sudo apt-get install g++ build-essential
-
 elm-package install -y
 
 elm-reactor -p 8001 &
+
 cd integration-tests
+
+sudo apt-get install g++ build-essential
+npm i -g node-gyp
 npm i
+
 npm test

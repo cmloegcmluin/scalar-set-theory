b18d195e0829aaf5a582529e7463dcd373f6ed31
Douglas Blumeyer <dblumeyer@pivotal.io>
2018-06-28 09:48:21 -0700

    crap

--- bin/ci.sh
+++ bin/ci.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+echo "installing all sorts of crap"
+apt-get update \
+  && apt-get install -y \
+    python \
+    gcc \
+    make \
+    g++ \
+    build-essential \
+  && npm i -g node-gyp
+  echo "finished installing crap"
+
 elm-package install -y
 
 elm-reactor -p 8001 &

adc520cb3a98b068c6875873e3e6791f980ec76d
Douglas Blumeyer <dblumeyer@pivotal.io>
2018-06-27 17:17:46 -0700

    fiddle with ci script

--- bin/ci.sh
+++ bin/ci.sh
@@ -2,9 +2,11 @@
 
 mkdir node_modules
 npm i elm
+export PATH=${PATH}:/home/travis/build/cmloegcmluin/scalar-set-theory/node_modules/.bin
 
 elm-package install -y
 
+elm-make src/ScalarSetTheory/Main.elm --output dist/index.html
 elm-reactor -p 8001 &
 cd integration-tests
 npm i
